903bb2169322600fdc7e5aa2473ea29f2b8445a8
Aseem Regmi <aseem.regmi22@gmail.com>
2018-09-02 15:58:00 +0545

    Added PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const { ObjectID } = require('mongodb');
 const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
 const { User } = require('./models/user');
+const _ = require('lodash');
+const bodyParser = require('body-parser');
 
 // Setup Port
 const port = process.env.PORT || 5500;
@@ -68,6 +70,33 @@ app.delete('/todos/:id', (req, res) => {
     .then(todo => {
       if (!todo) {
         return res.status(404).send();
+      }
+      res.send({ todo });
+    })
+    .catch(err => {
+      res.status(400).send();
+    });
+});
+
+app.patch('/todos/:id', (req, res) => {
+  const id = req.params.id;
+  let body = _.pick(req.body, ['text', 'completed']);
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+  console.log(body.completed);
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, { $set: body }, { new: true })
+    .then(todo => {
+      if (!todo) {
+        return res.status.status(404).send();
       }
       res.send({ todo });
     })

5eabf869c86f1cd9211ee5406773d480334264c8
Aseem Regmi <aseem.regmi22@gmail.com>
2018-09-02 17:37:56 +0545

    Some changes

--- server/config/config.js
+++ server/config/config.js
@@ -1,4 +1,5 @@
 const env = process.env.NODE_ENV || 'development';
+console.log(env);
 const localURL = 'mongodb://localhost:27017/';
 const mLabURL = 'mongodb://aseemregmi:aseemregmi123@ds143242.mlab.com:43242/';
 
@@ -8,6 +9,6 @@ if (env === 'development') {
 } else if (env === 'test') {
   process.env.PORT = 3000;
   process.env.MONGODB_URI = `${localURL}TodoAppTest`;
-} else {
+} else if (env === 'production') {
   process.env.MONGODB_URI = `${mLabURL}TodoApp`;
 }

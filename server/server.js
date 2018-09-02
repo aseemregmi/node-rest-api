4860ed166143f3c959f1c5a29496f06ab2344b22
Aseem Regmi <aseem.regmi22@gmail.com>
2018-09-02 14:52:28 +0545

    Test the DELETE /todos/:id

--- server/server.js
+++ server/server.js
@@ -65,11 +65,11 @@ app.delete('/todos/:id', (req, res) => {
   }
 
   Todo.findByIdAndRemove(id)
-    .then(doc => {
-      if (!doc) {
+    .then(todo => {
+      if (!todo) {
         return res.status(404).send();
       }
-      res.send(doc);
+      res.send({ todo });
     })
     .catch(err => {
       res.status(400).send();

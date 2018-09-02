a94afb4789fdd289e75612b0ffcd6b58cc215d22
Aseem Regmi <aseem.regmi22@gmail.com>
2018-09-02 13:46:28 +0545

    Setup app for heroku and mLab

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 mongoose.connect(
-  'mongodb://localhost:27017/TodoApp',
+  'mongodb://aseemregmi:aseemregmi123@ds143242.mlab.com:43242/todoapp',
   { useNewUrlParser: true }
 );
 
